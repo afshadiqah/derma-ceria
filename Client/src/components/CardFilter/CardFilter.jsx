163df9e74d1b9941c6f58135ae59d1b47920ff3f
anzhanny <smzyn23@gmail.com>
2024-06-22 21:34:46 +0700

    addBE:Kampanye

--- Client/src/components/CardFilter/CardFilter.jsx
+++ Client/src/components/CardFilter/CardFilter.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Card, ProgressBar, Col } from "react-bootstrap";
+import { Card, ProgressBar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import styles from "../CardFilter/CardFilter.module.css";
 import ProfileComponent from "../ProfileComponent";
+import styles from "./CardFilter.module.css";
 
-const CardFilter = ({ imageSrc, label, title, dari, progress, terkumpul, LinkButton }) => {
+const CardFilter = ({ imageSrc, kategori, title, target, terkumpul, LinkButton }) => {
+  // Hitung persentase progres
+  const progress = target !== 0 ? (terkumpul / target) * 100 : 0;
+
   return (
     <div>
       <Card className={styles.CardImg}>
@@ -12,27 +15,26 @@ const CardFilter = ({ imageSrc, label, title, dari, progress, terkumpul, LinkBut
           <Card.Img variant="top" src={imageSrc} />
         </div>
         <Card.Body>
-
           <div className={styles.labelCard}>
-          <Card.Text>{label}</Card.Text>
+            <Card.Text>{kategori}</Card.Text>
           </div>
           <div className={styles.titleCard}>
             <Card.Text>{title}</Card.Text>
           </div>
           <div className={styles.progressBar}>
-            <ProgressBar animated now={progress} variant="warning" style={{ width: "100%", height: "8px" }} />
+            <ProgressBar animated now={progress} variant="warning" style={{ height: "10px", fontSize: "0.75rem" }} />
           </div>
           <div className={styles.amount}>
             <Card.Text>
-              <strong>Rp. {terkumpul}</strong> terkumpul dari Rp. {dari}
+              <strong>Rp {terkumpul ? terkumpul.toLocaleString() : 0}</strong> terkumpul dari Rp {target ? target.toLocaleString() : 0}
             </Card.Text>
           </div>
-            <div className={styles.profileAndButton}>
-              <ProfileComponent />
-              <Link to={LinkButton} className={`btn btn-warning ${styles.btnDonasi}`}>
-                Donasi
-              </Link>
-            </div>
+          <div className={styles.profileAndButton}>
+            <ProfileComponent />
+            <Link to={LinkButton} className={`btn btn-warning ${styles.btnDonasi}`}>
+              Donasi
+            </Link>
+          </div>
         </Card.Body>
       </Card>
     </div>
